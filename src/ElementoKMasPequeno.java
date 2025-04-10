d334dae2412a7c77e9c2a7f9c35a0988049b2732
EddyArag <164369960+EddyArag@users.noreply.github.com>
2025-04-09 21:43:44 -0500

    Act

--- src/ElementoKMasPequeno.java
+++ src/ElementoKMasPequeno.java
@@ -46,9 +46,9 @@ public class ElementoKMasPequeno {
 
     public static void main(String[] args) {
         // Ejemplos del enunciado
-        System.out.println(encontrarKthMasPequeño(new int[]{4, 2, 7, 10, 4, 17}, 3)); // 4
-        System.out.println(encontrarKthMasPequeño(new int[]{4, 2, 7, 10, 4, 1, 6}, 5)); // 6
-        System.out.println(encontrarKthMasPequeño(new int[]{4, 2, 7, 1, 4, 6}, 1)); // 1
-        System.out.println(encontrarKthMasPequeño(new int[]{9, 2, 7, 1, 7}, 4)); // 7
+        System.out.println(encontrarKthMasPequeño(new int[] { 4, 2, 7, 10, 4, 17 }, 3)); // 4
+        System.out.println(encontrarKthMasPequeño(new int[] { 4, 2, 7, 10, 4, 1, 6 }, 5)); // 6
+        System.out.println(encontrarKthMasPequeño(new int[] { 4, 2, 7, 1, 4, 6 }, 1)); // 1
+        System.out.println(encontrarKthMasPequeño(new int[] { 9, 2, 7, 1, 7 }, 4)); // 7
     }
 }
